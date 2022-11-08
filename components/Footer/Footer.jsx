2af6cc19a56cf155a4ac5f4950a54104180d4de7
Jesse Akorah <jesseakorah@gmail.com>
2022-11-08 21:07:39 +0100

    Add header icons

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,10 +1,16 @@
 import Link from "next/link";
+import { MapPinIcon } from "@heroicons/react/24/outline";
+import { UserGroupIcon } from "@heroicons/react/24/outline";
+import { PhoneIcon } from "@heroicons/react/24/outline";
+import { DocumentTextIcon } from "@heroicons/react/24/outline";
 
 const style = {
   footer: `flex flex-col bg-[#212121] text-[#fff] pt-14 pb-10`,
   top: `flex container m-auto px-6 pb-8 justify-between`,
   topLeft: `flex`,
   listContainer: `w-48 text-xs`,
+  heroIcon: `h-4 w-4 mr-1`,
+  headerContainer: `flex`,
   listHeader: `font-bold uppercase mb-3`,
   listItem: `mb-3`,
   topRight: `flex flex-col justify-between items-end`,
@@ -21,14 +27,20 @@ const Footer = () => {
       <div className={style.top}>
         <div className={style.topLeft}>
           <div className={style.listContainer}>
-            <h3 className={style.listHeader}>Store Locator</h3>
+            <div className={style.headerContainer}>
+              <MapPinIcon className={style.heroIcon} />
+              <h3 className={style.listHeader}>Store Locator</h3>
+            </div>
             <ul>
               <li className={style.listItem}>Find a Boutique</li>
               <li className={style.listItem}>Book an Appointment</li>
             </ul>
           </div>
           <div className={style.listContainer}>
-            <h3 className={style.listHeader}>Customer Service</h3>
+            <div className={style.headerContainer}>
+              <PhoneIcon className={style.heroIcon} />
+              <h3 className={style.listHeader}>Customer Service</h3>
+            </div>
             <ul>
               <li className={style.listItem}>Exclusive Services</li>
               <li className={style.listItem}>Contact Us</li>
@@ -40,7 +52,10 @@ const Footer = () => {
             </ul>
           </div>
           <div className={style.listContainer}>
-            <h3 className={style.listHeader}>About Us</h3>
+            <div className={style.headerContainer}>
+              <UserGroupIcon className={style.heroIcon} />
+              <h3 className={style.listHeader}>About Us</h3>
+            </div>
             <ul>
               <li className={style.listItem}>Company Profile</li>
               <li className={style.listItem}>Corporate Data</li>
@@ -51,7 +66,10 @@ const Footer = () => {
             </ul>
           </div>
           <div className={style.listContainer}>
-            <h3 className={style.listHeader}>Legal</h3>
+            <div className={style.headerContainer}>
+              <DocumentTextIcon className={style.heroIcon} />
+              <h3 className={style.listHeader}>Legal</h3>
+            </div>
             <ul>
               <li className={style.listItem}>Legal Notes</li>
               <li className={style.listItem}>Terms & Conditions</li>
