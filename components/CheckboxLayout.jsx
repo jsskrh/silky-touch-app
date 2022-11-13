d3eca2e8646e8b321a7cfd50bc0d6e63d3395d2a
Jesse Akorah <jesseakorah@gmail.com>
2022-11-13 03:43:24 +0100

    Create checkbox layout component

---
+++ components/CheckboxLayout.jsx
@@ -0,0 +1,32 @@
+import { CheckIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+
+const style = {
+  checkboxWrapper: `inline-block relative`,
+  checkboxLabel: `flex items-center cursor-pointer`,
+  realCheckbox: `absolute w-0 opacity-0`,
+  fakeCheckbox: `border border-[#212121] h-4 w-4 mr-1 bg-[#eee]`,
+};
+
+const CheckboxLayout = ({ children, id }) => {
+  const [checkbox, setCheckbox] = useState(false);
+
+  return (
+    <div className={style.checkboxWrapper}>
+      <label htmlFor={id} className={style.checkboxLabel}>
+        <input
+          type="checkbox"
+          id={id}
+          className={style.realCheckbox}
+          onClick={() => setCheckbox(!checkbox)}
+        />
+        <div className={style.fakeCheckbox}>
+          {checkbox && <CheckIcon className={style.heroIcon} />}
+        </div>
+        <span>{children}</span>
+      </label>
+    </div>
+  );
+};
+
+export default CheckboxLayout;
--- components/Login/RememberMe.jsx
+++ components/Login/RememberMe.jsx
@@ -1,33 +1,13 @@
-import { CheckIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
+import CheckboxLayout from "../CheckboxLayout";
 
 const style = {
   checkboxContainer: `mb-9`,
-  checkboxWrapper: `inline-block relative`,
-  rememberMeLabel: `flex items-center cursor-pointer`,
-  realCheckbox: `absolute w-0 opacity-0`,
-  fakeCheckbox: `border border-[#212121] h-4 w-4 mr-1 bg-[#eee]`,
 };
 
 const RememberMe = () => {
-  const [checkbox, setCheckbox] = useState(false);
-
   return (
     <div className={style.checkboxContainer}>
-      <div className={style.checkboxWrapper}>
-        <label htmlFor="rememberMe" className={style.rememberMeLabel}>
-          <input
-            type="checkbox"
-            id="rememberMe"
-            className={style.realCheckbox}
-            onClick={() => setCheckbox(!checkbox)}
-          />
-          <div className={style.fakeCheckbox}>
-            {checkbox && <CheckIcon className={style.heroIcon} />}
-          </div>
-          <span>Remember me</span>
-        </label>
-      </div>
+      <CheckboxLayout id="rememberMe">Remember me</CheckboxLayout>
     </div>
   );
 };
