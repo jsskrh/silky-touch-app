b383dbca37d2848344a25cd904a9d1e0591c658c
Jesse Akorah <jesseakorah@gmail.com>
2022-11-29 00:33:44 +0100

    Fix convert date issue

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -82,7 +82,7 @@ const OrderPage = () => {
     const convertTime = (date) => {
       var hours = date.getHours();
       var minutes = date.getMinutes();
-      var ampm = hours >= 12 ? "AM" : "PM";
+      var ampm = hours <= 12 ? "AM" : "PM";
       hours = hours % 12;
       hours = hours ? hours : 12; // the hour '0' should be '12'
       var minCont = minutes < 10 ? "0" + minutes : minutes;
