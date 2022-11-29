ba3d379424015ed410021e2310916360b4382423
Jesse Akorah <jesseakorah@gmail.com>
2022-11-29 14:34:31 +0100

    Update product model

--- models/product.js
+++ models/product.js
@@ -4,10 +4,12 @@ const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     slug: { type: String, required: true, unique: true },
-    category: { type: String, required: true },
     images: { type: Array, required: true },
     price: { type: Number, required: true },
     brand: { type: String, required: true },
+    category: { type: String, required: true },
+    subcategory: { type: String, required: true },
+    subSubcategory: { type: String, required: true },
     color: { type: [String], required: true },
     countInStock: { type: Number, required: true, default: 0 },
     description: { type: String, required: true },
