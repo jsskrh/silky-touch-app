2b7181287104d33967760c27d19d32c4b266059c
Jesse Akorah <jesseakorah@gmail.com>
2022-12-14 17:18:56 +0100

    Pin product content to screen

--- pages/[category]/[subcategory]/[subSubcategory]/[slug].jsx
+++ pages/[category]/[subcategory]/[subSubcategory]/[slug].jsx
@@ -1,4 +1,7 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
 import Layout from "../../../../components/Layout/Layout";
 import PageNavigation from "../../../../components/ProductPage/PageNavigation";
 import ProductContent from "../../../../components/ProductPage/ProductContent";
@@ -7,7 +10,7 @@ import Product from "../../../../models/product";
 import db from "../../../../utils/db";
 
 const style = {
-  pdpTop: `md:flex`,
+  pdpTop: `md:flex `,
   pdpLeft: `flex-1`,
 };
 
@@ -24,14 +27,49 @@ const productPage = ({ product }) => {
     );
   }
 
+  const [tabState, setTabState] = useState(0);
+
+  const productPageRef = useRef();
+  const productContentRef = useRef();
+  const productAllRef = useRef();
+
+  useEffect(() => {
+    const context = productPageRef.current;
+
+    let testHeight = productContentRef.current.clientHeight;
+
+    let totalScroll = productAllRef.current.clientHeight - testHeight;
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    let ctx = gsap.context(() => {
+      ScrollTrigger.create({
+        trigger: productAllRef.current,
+        pin: productContentRef.current,
+        scrub: true,
+        start: "top 88px",
+        end: "+=" + totalScroll,
+      });
+    }, context);
+
+    return () => {
+      ctx.revert();
+    };
+  }, [tabState]);
+
   return (
     <Layout title={product.name} path={path} productPage>
-      <div className={style.pdpMain}>
-        <div className={style.pdpTop}>
+      <div className={style.pdpMain} ref={productPageRef}>
+        <div className={style.pdpTop} ref={productAllRef}>
           <div className={style.pdpLeft}>
             <ProductImages product={product} />
           </div>
-          <ProductContent product={product} />
+          <ProductContent
+            product={product}
+            tabState={tabState}
+            setTabState={setTabState}
+            ref={productContentRef}
+          />
         </div>
 
         <div className={style.recommendationsBlock}></div>
