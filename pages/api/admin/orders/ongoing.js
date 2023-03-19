cbc7e07f7049dac936444d7d6df3d6242302bd32
Jesse Akorah <jesse.akorah@sage-grey.com>
2023-03-19 01:50:53 +0100

    Add admin page for ongoing orders

---
+++ pages/api/admin/orders/ongoing.js
@@ -0,0 +1,32 @@
+import { getSession } from "next-auth/react";
+import Order from "../../../../models/order";
+import User from "../../../../models/user";
+import db from "../../../../utils/db";
+
+const handler = async (req, res) => {
+  const session = await getSession({ req });
+  if (!session) {
+    return res.status(401).send({ message: "Sign in required" });
+  }
+
+  const { user } = session;
+  if (!user.isAdmin) {
+    return res.status(401).send({ message: "Sign in required" });
+  }
+
+  await db.connect();
+  const orders = await Order.find({ isDelivered: false });
+  const populatedOrders = await Promise.all(
+    orders.map(async (order) => {
+      const user = await User.findById(order.user);
+      return {
+        ...order.toJSON(),
+        user: user.name,
+      };
+    })
+  );
+  await db.disconnect();
+  res.send(populatedOrders);
+};
+
+export default handler;
