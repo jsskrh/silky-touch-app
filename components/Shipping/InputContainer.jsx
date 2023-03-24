a652bc0175c5fdf0bdbd34daa444a9f2d99d9088
Jesse Akorah <jesse.akorah@sage-grey.com>
2023-03-24 20:23:46 +0100

    Add extra style

--- components/Shipping/InputContainer.jsx
+++ components/Shipping/InputContainer.jsx
@@ -1,13 +1,14 @@
 import InputLabel from "./InputLabel";
 
 const style = {
-  inputContainer: `mb-6 md:w-1/2`,
+  inputContainer: `mb-6`,
+  halfWidth: `md:w-1/2`,
   errorMessage: `mt-1 text-[#bf2d2d]`,
 };
 
-const InputContainer = ({ children, id, label, error }) => {
+const InputContainer = ({ children, id, label, error, fullWidth }) => {
   return (
-    <div className={style.inputContainer}>
+    <div className={`${style.inputContainer} ${!fullWidth && style.halfWidth}`}>
       <InputLabel id={id} text={label} required />
       {children}
       {error && <div className={style.errorMessage}>{error}</div>}
