bd96964bdec02e821c80d9ec76805cc9785a2f3b
Jesse Akorah <jesse.akorah@sage-grey.com>
2023-03-24 20:20:16 +0100

    Add extra validation to user model

--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true, trim: true, minLength: 6 },
     isAdmin: { type: Boolean, required: true, default: false },
   },
   { timestamps: true }
