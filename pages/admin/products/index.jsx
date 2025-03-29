d2cf8e3a9cf23dbac6fb7b63dbfdc5aed0165999
Jesse Akorah <jesseakorah@gmail.com>
2025-03-29 14:24:15 +0100

    Create admin all products page

---
+++ pages/admin/products/index.jsx
@@ -0,0 +1,56 @@
+import { useRouter } from "next/router";
+import Layout from "../../../components/Layout/Layout";
+import ProductItem from "../../../components/ProductCatalogue/ProductItem";
+import Product from "../../../models/product";
+import db from "../../../utils/db";
+import QueryBar from "../../../components/ProductCatalogue/QueryBar";
+
+const style = {
+  productsGrid: `pt-4 mx-4 grid grid-cols-2 gap-1 md:grid-cols-3 lg:grid-cols-4`,
+};
+
+const products = ({ products }) => {
+  const router = useRouter();
+
+  // Fix path error in console
+  const path = router.asPath;
+
+  const productsMeta = {
+    title: "Men",
+    subtitle:
+      "Get the quintessential Silky Touch look by pairing the latest runway styles with a statement bag and modern shoes. For the finishing touch, browse an array of luxurious accessories.",
+  };
+
+  return (
+    <Layout
+      path={path}
+      title={productsMeta.title}
+      subtitle={productsMeta.subtitle}
+      productsCatalogue
+    >
+      <QueryBar productNo={products.length} />
+      <div className={style.productsGrid}>
+        {products.map((product) => (
+          <ProductItem product={product} key={product._id} />
+        ))}
+      </div>
+    </Layout>
+  );
+};
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+
+  await db.connect();
+  const products = await Product.find().sort({ createdAt: -1 }).lean();
+
+  const productsStringified = products.map(db.stringifyProducts);
+
+  return {
+    props: {
+      products: productsStringified.map(db.convertDocsToObj),
+    },
+  };
+}
+
+export default products;
