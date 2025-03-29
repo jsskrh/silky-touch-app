183f5c253b575149e469f9eeeb4b317575f618d5
Jesse Akorah <jesseakorah@gmail.com>
2025-03-29 14:24:57 +0100

    Update brand grid images

--- components/Home/BrandGrid.jsx
+++ components/Home/BrandGrid.jsx
@@ -13,12 +13,39 @@ const style = {
   button: `transition-all border px-[30px] py-[13px] w-52 text-xs font-bold uppercase mt-4 bg-[#212121] border-[#212121] text-[#ededed] hover:bg-[#000] hover:border-[#000] hover:text-[#fff]`,
 };
 
+const images = [
+  "https://res.cloudinary.com/dixuzyoht/image/upload/v1743239524/homepage/etpmi3bpogqpljsnne8j.jpg",
+  "https://res.cloudinary.com/dixuzyoht/image/upload/v1743239524/homepage/wxt9yirt0dslnmhjjgol.jpg",
+  "https://res.cloudinary.com/dixuzyoht/image/upload/v1743239524/homepage/p57d1utnmwevdn8biuxi.jpg",
+  "https://res.cloudinary.com/dixuzyoht/image/upload/v1743239524/homepage/ym0kyuelae1f1t4rdygw.jpg",
+];
+
 const BrandGrid = ({ activeBrand, brand }) => {
   return (
     <div className={style.sectionContainer}>
       <BrandGridSlider brand={activeBrand} />
       <div className={style.brandGrid}>
-        {activeBrand.map((product, index) => (
+        {images.map((image, index) => (
+          <div className={style.imageContainer} key={index}>
+            <img src={image} alt={index} className={style.image} />
+            {/* <Image
+                unoptimized
+                src={
+                  product.images.find(
+                    (image) =>
+                      image.type === "model showcase front" ||
+                      image.type === "model front" ||
+                      image.type === "secondary"
+                  ).url
+                }
+                alt={product.name}
+                className={style.image}
+                style={{ maxWidth: "100%" }}
+                fill
+              /> */}
+          </div>
+        ))}
+        {/* {activeBrand.map((product, index) => (
           <Link
             href={`/${product.category}/${product.subcategory}/${product.subSubcategory}/${product._id}`}
             key={index}
@@ -36,24 +63,9 @@ const BrandGrid = ({ activeBrand, brand }) => {
                 alt={product.name}
                 className={style.image}
               />
-              {/* <Image
-                unoptimized
-                src={
-                  product.images.find(
-                    (image) =>
-                      image.type === "model showcase front" ||
-                      image.type === "model front" ||
-                      image.type === "secondary"
-                  ).url
-                }
-                alt={product.name}
-                className={style.image}
-                style={{ maxWidth: "100%" }}
-                fill
-              /> */}
             </div>
           </Link>
-        ))}
+        ))} */}
       </div>
       <div className={style.brandDetails}>
         <h2 className={style.title}>Fashion</h2>
--- pages/index.jsx
+++ pages/index.jsx
@@ -56,44 +56,49 @@ export default function Home({ activeBrand }) {
 export async function getServerSideProps() {
   await db.connect();
 
-  const product1 = await Product.findById("64b950130ef0988aa23b2e34").lean();
-  const product2 = await Product.findById("64b94825663c4d77d584240c").lean();
-  const product3 = await Product.findById("64b960543fdaf4906c52767e").lean();
-  const product4 = await Product.findById("64b955470ef0988aa23b2e6f").lean();
-  const product5 = await Product.findById("64bc3ba80a25fdbbe406bf3f").lean();
-  const product6 = await Product.findById("6567845d29104c1ffa4c7d55").lean();
-  const product7 = await Product.findById("6567839ad6c0644ab3188f4e").lean();
-  const product8 = await Product.findById("6567802f29104c1ffa4c7d49").lean();
-  const product9 = await Product.findById("65dd9e19f6bc3c36359d3a29").lean();
-  const product10 = await Product.findById("6567824129104c1ffa4c7d4e").lean();
-  const product11 = await Product.findById("65e6af0947e8417038aaf433").lean();
-  const product12 = await Product.findById("65e5c33b5d1823422aaff82c").lean();
-  const product13 = await Product.findById("65e5b7b8ec3bb9ab3ea3143d").lean();
-  const product14 = await Product.findById("65e58b657b3e1fd8cf8b2fba").lean();
-  const product15 = await Product.findById("65e5653423263892ce683e75").lean();
-  const product16 = await Product.findById("65e55e1323263892ce683e63").lean();
-  const product17 = await Product.findById("65e55ce623263892ce683e5a").lean();
-  const product18 = await Product.findById("65e567ac23263892ce683e7c").lean();
+  const product1 = await Product.findById("67e431cdf87bc4b60a4c9670").lean();
+  const product2 = await Product.findById("64b960543fdaf4906c52767e").lean();
+  const product3 = await Product.findById("65e58c8b7b3e1fd8cf8b2fbf").lean();
+  const product4 = await Product.findById("64b98dff3fdaf4906c5276fb").lean();
+
+  // const product1 = await Product.findById("64b950130ef0988aa23b2e34").lean();
+  // const product2 = await Product.findById("64b94825663c4d77d584240c").lean();
+  // const product3 = await Product.findById("64b960543fdaf4906c52767e").lean();
+  // const product4 = await Product.findById("64b955470ef0988aa23b2e6f").lean();
+  // const product5 = await Product.findById("64bc3ba80a25fdbbe406bf3f").lean();
+  // const product6 = await Product.findById("6567845d29104c1ffa4c7d55").lean();
+  // const product7 = await Product.findById("6567839ad6c0644ab3188f4e").lean();
+  // const product8 = await Product.findById("6567802f29104c1ffa4c7d49").lean();
+  // const product9 = await Product.findById("65dd9e19f6bc3c36359d3a29").lean();
+  // const product10 = await Product.findById("6567824129104c1ffa4c7d4e").lean();
+  // const product11 = await Product.findById("65e6af0947e8417038aaf433").lean();
+  // const product12 = await Product.findById("65e5c33b5d1823422aaff82c").lean();
+  // const product13 = await Product.findById("65e5b7b8ec3bb9ab3ea3143d").lean();
+  // const product14 = await Product.findById("65e58b657b3e1fd8cf8b2fba").lean();
+  // const product15 = await Product.findById("65e5653423263892ce683e75").lean();
+  // const product16 = await Product.findById("65e55e1323263892ce683e63").lean();
+  // const product17 = await Product.findById("65e55ce623263892ce683e5a").lean();
+  // const product18 = await Product.findById("65e567ac23263892ce683e7c").lean();
 
   const activeBrand = [
     product1,
     product2,
     product3,
     product4,
-    product5,
-    product6,
-    product7,
-    product8,
-    product9,
-    product10,
-    product11,
-    product12,
-    product13,
-    product14,
-    product15,
-    product16,
-    product17,
-    product18,
+    // product5,
+    // product6,
+    // product7,
+    // product8,
+    // product9,
+    // product10,
+    // product11,
+    // product12,
+    // product13,
+    // product14,
+    // product15,
+    // product16,
+    // product17,
+    // product18,
   ];
 
   const activeBrandStringified = activeBrand.map(db.stringifyProducts);
